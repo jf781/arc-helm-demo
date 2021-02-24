597270cabafdce41594a051a477e87ee1c1b0f57
jf781 <50719731+jf781@users.noreply.github.com>
2021-02-24 08:40:02 -0600

    Added Shark Node.js App

---
+++ app/app.js
@@ -0,0 +1,33 @@
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+
+let appInsights = require('applicationinsights');
+appInsights.setup();
+appInsights.start();
+
+const express = require('./node_modules/express');
+const app = express();
+const router = express.Router();
+
+const path = __dirname + '/views/';
+const port = 8080;
+
+router.use(function (req,res,next) {
+  console.log('/' + req.method);
+  next();
+});
+
+router.get('/', function(req,res){
+  res.sendFile(path + 'index.html');
+});
+
+router.get('/sharks', function(req,res){
+  res.sendFile(path + 'sharks.html');
+});
+
+app.use(express.static(path));
+app.use('/', router);
+
+app.listen(port, function () {
+  console.log('Example app listening on port 8080!')
+})
